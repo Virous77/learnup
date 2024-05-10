56b06614d82221c7f8c7f31d5b23de2cd1d6be67
Reetesh Kumar <rajreetesh7@gmail.com>
2024-05-11 00:23:14 +0530

    fixed password hash issue

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,17 @@
-export { auth as middleware } from "@/auth/auth";
+export { auth } from "@/auth/auth";
+import { NextRequest, NextResponse } from "next/server";
+const protectedRoutes = ["/"];
+
+export async function middleware(req: NextRequest) {
+  const session = req.cookies.get("authjs.session-token")?.value;
+
+  if (!session && protectedRoutes.includes(req.nextUrl.pathname)) {
+    const url = new URL("/login", req.nextUrl.origin);
+    return NextResponse.redirect(url.toString());
+  }
+
+  if (session && req.nextUrl.pathname === "/login") {
+    const url = new URL("/", req.nextUrl.origin);
+    return NextResponse.redirect(url.toString());
+  }
+}
